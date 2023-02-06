e9375bb6d7d04337ffb6e2d70f2cf11683573213
ethersloth <ramboy17@hotmail.com>
2023-02-06 11:17:57 -0600

    fixing

--- kvm_vm_create.py
+++ kvm_vm_create.py
@@ -6,27 +6,27 @@ from shitbox import mount_drive
 
 def create_vm():
     name = input("Enter the name of the VM: ")
-    if os.isfile(mount_drive() + name + ".qcow2"):
+    if os.path.isfile(mount_drive() + name + ".qcow2"):
         print("VM already exists.")
         name = input("Enter the name of the VM: ")
     ram = input("Enter the amount of RAM (in MB): ")
     if ram.isdigit():
         print("Invalid RAM amount.")
         ram = input("Enter the amount of RAM (in MB): ")
-    elif ram < 1024:
+    elif ram < "1024":
         print("RAM amount is too low.")
         ram = input("Enter the amount of RAM (in MB): ")
-    elif ram > 4096:
+    elif ram > "4096":
         print("RAM amount is too high.")
         ram = input("Enter the amount of RAM (in MB): ")
     cpu = input("Enter the number of CPUs: ")
     if cpu.isdigit():
         print("Invalid CPU amount.")
         cpu = input("Enter the number of CPUs: ")
-    elif cpu < 1:
+    elif cpu < "1":
         print("CPU amount is too low.")
         cpu = input("Enter the number of CPUs: ")
-    elif cpu > 4:
+    elif cpu > "4":
         print("CPU amount is too high.")
         cpu = input("Enter the number of CPUs: ")
     os_type = input("Enter the OS type: ")
@@ -34,13 +34,17 @@ def create_vm():
     location = mount_drive() + name + ".iso"
     vm_image_location = mount_drive() + name + ".qcow2"
 
-    os.system("virt-install --name={} --ram={} --vcpus={} --disk path={},size=100 --os-type={} --os-variant={} --network bridge=br0 --graphics vnc,listen=5900 --console pty,target_type=serial --location '{}' --extra-args 'console=ttyS0,115200n8 serial' --noreboot".format(name, ram, cpu, vm_image_location, os_type, os_variant, location))
+    os.system(
+        "virt-install --name={} --ram={} --vcpus={} --disk path={},size=100 --os-type={} --os-variant={} --network bridge=br0 --graphics vnc,listen=5900 --console pty,target_type=serial --location '{}' --extra-args 'console=ttyS0,115200n8 serial' --noreboot".format(
+            name, ram, cpu, vm_image_location, os_type, os_variant, location))
 
     print("VM created successfully.")
 
 
 # Main
-create_vm()
+def main():
+    create_vm()
+
 
 if __name__ == "__main__":
-    create_vm()
+    main()
